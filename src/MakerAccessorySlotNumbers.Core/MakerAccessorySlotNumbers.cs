37b10dbb85e833420d118283d13d972feb6b3093
Njaecha <andre.grabowich@gmail.com>
2024-05-07 00:23:32 +0200

    Backport to KK

--- src/MakerAccessorySlotNumbers.Core/MakerAccessorySlotNumbers.cs
+++ src/MakerAccessorySlotNumbers.Core/MakerAccessorySlotNumbers.cs
@@ -28,13 +28,20 @@ namespace MakerAccessorySlotNumbers
         public static IEnumerable<CodeInstruction> UpdateSlotNameTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
             List<CodeInstruction> code = new List<CodeInstruction>(instructions);
-
             CodeMatcher cm = new CodeMatcher(instructions, generator);
+#if KKS
             cm.MatchForward(false, new CodeMatch(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(ChaInfo), nameof(ChaInfo.infoAccessory))));
             cm.Advance(-1);
             cm.RemoveInstructions(9);
             cm.InsertAndAdvance(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Hooks), nameof(SlotNameUpdater))));
-
+#elif KK
+            cm.MatchForward(false, new CodeMatch(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(ListInfoBase), nameof(ListInfoBase.Name))));
+            cm.Advance(-7);
+            cm.RemoveInstructions(9);
+            cm.InsertAndAdvance(
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Hooks), nameof(SlotNameUpdater)))
+            );
+#endif
             return cm.Instructions();
         }
 
@@ -44,30 +51,47 @@ namespace MakerAccessorySlotNumbers
             List<CodeInstruction> code = new List<CodeInstruction>(instructions);
 
             CodeMatcher cm = new CodeMatcher(instructions, generator);
+#if KKS
             cm.MatchForward(false, new CodeMatch(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(ChaInfo), nameof(ChaInfo.infoAccessory))));
             cm.Advance(-1);
             cm.RemoveInstructions(2);
             cm.Advance(2);
             cm.RemoveInstructions(5);
             cm.InsertAndAdvance(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Hooks), nameof(SlotNameUpdater2))));
-
+#elif KK
+            cm.MatchForward(false, new CodeMatch(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(ListInfoBase), nameof(ListInfoBase.Name))));
+            cm.Advance(-8);
+            cm.RemoveInstructions(10);
+            cm.InsertAndAdvance(
+                new CodeInstruction(OpCodes.Ldloc_0),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Hooks), nameof(SlotNameUpdater2)))
+            );
+#endif
             return cm.Instructions();
         }
 
         public static void SlotNameUpdater(CvsAccessory cvs)
         {
+#if KKS
             cvs.chaCtrl.infoAccessory.SafeProc(cvs.nSlotNo, delegate (ListInfoBase info)
             {
                 cvs.textSlotName.text = (cvs.nSlotNo + 1).ToString("00") + " " + info.Name;
             });
+#elif KK
+            cvs.textSlotName.text = (cvs.nSlotNo + 1).ToString("00") + " " + cvs.chaCtrl.infoAccessory[cvs.nSlotNo].Name;
+#endif
         }
 
         public static void SlotNameUpdater2(CustomAcsChangeSlot cacs, int i)
         {
+#if KKS
             cacs.chaCtrl.infoAccessory.SafeProc(i, delegate (ListInfoBase info)
             {
                 cacs.textSlotNames[i].text = (i + 1).ToString("00") + " " + info.Name;
             });
+#elif KK   
+            cacs.textSlotNames[i].text = (i + 1).ToString("00") + " " + cacs.chaCtrl.infoAccessory[i].Name;
+#endif
         }
     }
 }
